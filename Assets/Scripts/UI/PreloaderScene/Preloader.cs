4a3969766e2c594d6a5cf9ce83d19b6473888f39
MuninYar <rezchikov.md@gmail.com>
2022-11-19 11:27:32 +0400

    Added preload & MainMenu scenes

---
+++ Assets/Scripts/UI/PreloaderScene/Preloader.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+
+public sealed class Preloader : MonoBehaviour
+{
+    private CanvasGroup _fadeGroup;
+    private float _loadTime;
+    private float _minLogoTime = 3;
+
+    private void Start()
+    {
+        _fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        _fadeGroup.alpha = 1;
+        
+        //Preload data by Task here. Get _loadTime
+        _loadTime = 0;
+
+        if (Time.time < _minLogoTime)
+        {
+            _loadTime = _minLogoTime;
+        }
+        else
+        {
+            _loadTime = Time.time; 
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Time.time < _minLogoTime)
+        {
+            _fadeGroup.alpha = 1 - Time.time;
+        }
+
+        if (Time.time > _minLogoTime && _loadTime != 0)
+        {
+            _fadeGroup.alpha = Time.time - _minLogoTime;
+            if (_fadeGroup.alpha >= 1)
+            {
+                Debug.Log("NextScene");
+            }
+        }
+    }
+}
